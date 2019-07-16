07763d509df8787e7835a1658b90bcf645b1df8c
Danze <hoangvpse130555@fpt.edu.vn>
2019-07-16 16:26:44 +0700

    small fix for nav of company

--- Areas/Company/Components/NavigationBarEmp.cs
+++ Areas/Company/Components/NavigationBarEmp.cs
@@ -21,20 +21,11 @@ namespace ZTourist.Areas.Company.Components
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            IEnumerable<Tour> popularTours = await touristDAL.GetTrendingToursAsync();
-            if (popularTours != null)
-            {
-                foreach (Tour tour in popularTours)
-                {
-                    tour.Destinations = await touristDAL.GetDestinationsByTourIdAsync(tour.Id);
-                }
-            }
             AppUser user = User?.Identity?.Name == null ? null : await userManager.FindByNameAsync(User.Identity.Name);
             string avatar = "https://ztourist.blob.core.windows.net/others/avatar.png";
             if (user != null)
                 avatar = user.Avatar;
             NavigationViewModel model = new NavigationViewModel {
-                PopularTours = popularTours,
                 Avatar = avatar
             };
             return View(model);
